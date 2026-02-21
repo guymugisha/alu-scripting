ebe6881f837d72011664f831729b337fb3dfb2df
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-21 20:27:05 +0200

    Refactored API request URL and headers

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,20 +5,16 @@ import requests
 
 def top_ten(subreddit):
     """Queries Reddit API and prints titles of first 10 hot posts."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "python:subreddit.top_ten:v1.0"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozila/5.0"}
     params = {"limit": 10}
 
     response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False,
-            timeout=5
+            url, headers=headers, params=params, allow_redirects=False
     )
 
     if response.status_code != 200:
-        print(response.status_code)
+        print(None)
         return None
 
     data = response.json()
